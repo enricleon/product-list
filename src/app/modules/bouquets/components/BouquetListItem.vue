9a3719b151c92e49b0e86fe74a1e33047be50d2d
Enric Leon <enricleonfont@gmail.com>
2019-10-31 19:27:43 +0100

    Add name and price to bouquet component

--- src/app/modules/bouquets/components/BouquetListItem.vue
+++ src/app/modules/bouquets/components/BouquetListItem.vue
@@ -1,5 +1,11 @@
 <template>
-    <img :src="bouquet.image" :title="bouquet.name">
+    <article class="bouquet">
+        <img class="bouquet-image" :src="bouquet.image" :title="bouquet.name">
+        <div class="bouquet-details">
+            <h3 class="bouquet-name">{{ bouquet.name }}</h3>
+            <div class="bouquet-price">{{ bouquet.price }}</div>
+        </div>
+    </article>
 </template>
 <script>
 export default {
