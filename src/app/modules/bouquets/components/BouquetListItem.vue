b465f66e66b8f1ab23b04c07b49e4de434091f3e
Enric Leon <enricleonfont@gmail.com>
2019-10-31 19:43:14 +0100

    Add some styling to the bouquet component

--- src/app/modules/bouquets/components/BouquetListItem.vue
+++ src/app/modules/bouquets/components/BouquetListItem.vue
@@ -2,7 +2,7 @@
     <article class="bouquet">
         <img class="bouquet-image" :src="bouquet.image" :title="bouquet.name">
         <div class="bouquet-details">
-            <h3 class="bouquet-name">{{ bouquet.name }}</h3>
+            <h4 class="bouquet-name">{{ bouquet.name }}</h4>
             <div class="bouquet-price">{{ bouquet.price }}</div>
         </div>
     </article>
@@ -17,6 +17,30 @@ export default {
   }
 };
 </script>
-<style lang="sass" scoped>
+<style lang="css" scoped>
+    .bouquet {
+        margin: 1rem;
+    }
 
+    .bouquet-image {
+        max-width: 100%;
+        background-color: white;
+        box-shadow: 0px 1px 3px #efefef;
+        border-radius: 5px;
+        margin-top: 5px;
+        transition: all 700ms;
+    }
+
+    .bouquet-image:hover {
+        margin-top: -5px;
+        cursor: pointer;
+    }
+
+    .bouquet-details {
+        padding: 1rem;
+    }
+
+    .bouquet-price {
+        font-size: .9rem;
+    }
 </style>
